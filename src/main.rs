021b97f0e0b0d374dbbf1b7734b9111ec6942150
KeyboardFire <andy@keyboardfire.com>
2016-04-14 19:24:15 -0500

    Make usage of Babble struct less obnoxious

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate babble;
+use babble::babble::Babble;
 
 fn main() {
-    let mut b = babble::babble::Babble::new();
+    let mut b = Babble::new();
     b.run("ARRHZLEZLLZLLZLOZTBTZTBHWZLOZLRZLLZLDZTBIZEPUT".to_string());
     b.run("ONEPVBONEPVAADDPVCPUT".to_string());
     b.run("PVAFIVPVBTWOPVASUBPVCPUT".to_string());
